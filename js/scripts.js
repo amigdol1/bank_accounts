6dfe2e716039875aaa4b34e4c0fa4eae1872b675
Alex Migdol and Zachary Richards <zachary@>
2016-07-06 19:28:59 -0700

    add the back-end and front-end logic for new deposit and withdraw

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ function initialSignUp(name, deposit) {
   this.deposit = deposit;
 }
 
+function balance(new_deposit, withdraw) {
+  this.newDeposit = new_deposit;
+  this.withdraw = withdraw;
+}
+
 $(document).ready(function() {
   $("form#bank_account").submit(function(event){
     event.preventDefault();
@@ -13,7 +18,12 @@ $(document).ready(function() {
     console.log(newAccount);
   });
 
+  $("form#deposit_withdraw").submit(function(event){
+      event.preventDefault();
+    var inputNewDeposit = parseInt($("input#deposit").val());
+    var inputWithdraw = parseInt($("input#withdraw").val());
+    var newBalance = new balance(inputNewDeposit, inputWithdraw);
 
-
-
+    console.log(newBalance);
+  });
 });
